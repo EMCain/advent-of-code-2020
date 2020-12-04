6a3f4c672d5c0815553d1533e9a9c6c2368d162c
EMCain <11471197+EMCain@users.noreply.github.com>
2020-12-04 11:10:34 -0800

    Day 4 part 1, not working

---
+++ 04a.rb
@@ -0,0 +1,48 @@
+REQUIRED_FIELDS = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+
+def get_passports(filename)
+    passports = Array.new
+    current_passport = ""
+    File.readlines(filename, chomp: true).each do |line|
+        stripped_line = line.strip 
+        if stripped_line == ""
+            passports.push(current_passport)
+            current_passport = ""
+        else
+            current_passport += " #{stripped_line} "
+        end
+    end
+    return passports
+end
+
+def validate_passports(passports)
+    valid_count = 0
+    passports.each do | p |
+        valid = 1 
+        missing = Array.new
+        REQUIRED_FIELDS.each do | field |
+            if ! p.include? field
+                valid = 0
+                missing.push(field)
+            end
+        end
+        if valid == 0
+            puts "#{p} is missing #{missing.join(", ")}"
+        end
+        valid_count += valid
+    end
+    return valid_count
+end
+
+def get_answer(filename)
+    passports = get_passports(filename)
+    answer = validate_passports(passports)
+    puts "valid count: #{answer}"
+end
+
+challenge_name = "04a"
+
+puts "test:"
+get_answer("inputs/#{challenge_name}_test.txt")
+puts "actual:"
+get_answer("inputs/#{challenge_name}_real.txt")
