c3509069a7d00c5bc032f4f111103673e33e4c89
EMCain <11471197+EMCain@users.noreply.github.com>
2020-12-04 15:52:04 -0800

    try validating, it's not working

--- 04b.rb
+++ 04b.rb
@@ -4,12 +4,11 @@ def get_passport_hash(passport_string)
     result = Hash.new
     result[:complete] = true
     REQUIRED_FIELDS.each do | field |
-        match = /#{field}:(#?\w+)/.match(passport_string)
+        match = /#{field}:(#?\w+[cm|in]?)/.match(passport_string)
         if match == nil
             result[:complete] = false
-            puts "#{field} missing from #{passport_string}"
         else
-            result[field] = match
+            result[field] = match[1]
         end
     end 
     return result 
@@ -32,26 +31,71 @@ def get_passports(filename)
     end
     passport_hash = get_passport_hash(current_passport_string)
     passports.push(passport_hash)
+    puts "#{passports.length} total passports"
     return passports
 end
 
-def validate_passports(passports)
-    passports.each do | p |
-        puts p 
+def validate_field(fieldname, value)
+    begin
+        case fieldname
+        when "byr"
+            return value.to_i.between?(1920, 2002)
+        when "iyr"
+            return value.to_i.between?(2010, 2020)
+        when "eyr"
+            return value.to_i.between?(2020, 2030)
+        when "hgt"
+            match = /^(?<num>\d+)(?<unit>\w+)?/.match(value)
+            if match[:unit] == "cm"
+                return match[:num].to_i.between?(150, 193)
+            elsif match[:unit] == "in"
+                return match[:num].to_i.between?(59, 76)
+            else
+                return false
+            end
+        when "hcl"
+            return /^#([\d|[a-f]]{6})$/.match(value) != nil
+        when "ecl"
+            return ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].include? value
+        when "pid"
+            return /^(\d{6})?/.match(value) != nil
+        when "cid"
+            return true
+        end
+    rescue
+        return false
     end
+end
+
+def validate_passports(passports)
+    valid_passports = 0
+    passports.each do | p |
+        valid = 1
+        REQUIRED_FIELDS.each do |field|
+            if not validate_field(field, p[field])
+                valid = 0
+            end
+        end
+        valid_passports += valid
+    end
+    return valid_passports
 end
 
 def get_answer(filename)
     passports = get_passports(filename)
     answer = validate_passports(passports)
-    # puts "valid count: #{answer}"
+    puts "valid count: #{answer}"
 end
 
 challenge_name = "04b"
 
 puts "test:"
 get_answer("inputs/#{challenge_name}_test.txt")
+
 # puts "fischer:" # asked a coworker to help by giving me the first 50 passwords and the results they got
 # get_answer("inputs/#{challenge_name}_fischer.txt")
-# puts "actual:"
-# get_answer("inputs/#{challenge_name}_real.txt")
+
+
+challenge_name = "04a"
+puts "actual:"
+get_answer("inputs/#{challenge_name}_real.txt")
